9ae199e0034e526e6d51d36ddbafa7b3d50709a5
shibatanaoto <naoto.shibata510@gmail.com>
2016-02-13 15:27:56 +0900

    setting

--- garden/settings_production.py
+++ garden/settings_production.py
@@ -18,7 +18,7 @@ from os.path import abspath, dirname
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # STATIC_ROOT
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
-
+STATIC_URL = '/static/'
 # プロジェクトのルートをSITE_ROOTとする
 SITE_ROOT = abspath(os.path.join(dirname(__file__), ".."))
 
